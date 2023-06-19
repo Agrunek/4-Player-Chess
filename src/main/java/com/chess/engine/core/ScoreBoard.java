d0ded816c4df1e873d1d2c6b9f094e099e2117f0
Adrian Antczak <adek24680@gmail.com>
2023-06-19 22:22:59 +0200

    Repairing scoreboard padding

--- src/main/java/com/chess/engine/core/ScoreBoard.java
+++ src/main/java/com/chess/engine/core/ScoreBoard.java
@@ -1,9 +1,12 @@
 package com.chess.engine.core;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
 import javafx.scene.text.Font;
 
 import java.io.File;
@@ -13,7 +16,7 @@ import static com.chess.engine.utils.Constants.Textures.FONT_PATH;
 
 public class ScoreBoard extends StackPane {
     private final Player player;
-    private final Label label = new Label();
+    private final Label score = new Label("0");
     private final Rectangle background = new Rectangle();
 
     public ScoreBoard(Player player) {
@@ -34,26 +37,35 @@ public class ScoreBoard extends StackPane {
 
         background.setFill(player.getColor().getColor());
 
-        label.setText("SCORE:" + player.getScore());
         Font customFont = Font.loadFont(new File(FONT_PATH).toURI().toString(), SCOREBOARD_SIZE / 5);
-        label.setFont(customFont);
-        label.setTextFill(player.getColor().getTextColor());
+        VBox vBox = new VBox();
+        Label heading = new Label("SCORE:");
+
+        heading.setFont(customFont);
+        score.setFont(customFont);
+
+        heading.setTextFill(player.getColor().getTextColor());
+        score.setTextFill(player.getColor().getTextColor());
+
+        vBox.getChildren().add(heading);
+        vBox.getChildren().add(score);
+        vBox.setAlignment(Pos.CENTER);
+
         getChildren().add(background);
-        getChildren().add(label);
+        getChildren().add(vBox);
     }
 
     public void updateScore() {
-        label.setText("SCORE:" + player.getScore());
+        score.setText(Integer.toString(player.getScore()));
     }
 
     public void highlightScore() {
         background.setStroke(player.getColor().getHighlightColor());
         background.setStrokeWidth(10);
+        background.setStrokeType(StrokeType.INSIDE);
     }
 
     public void noHighlightScore() {
         background.setStroke(Color.TRANSPARENT);
     }
 }
-
-
