06694a3247b9462f0eb7a6eb3dc668a4c734ad6b
Adrian Antczak <adek24680@gmail.com>
2023-06-20 16:40:24 +0200

    Main menu UI refactor

--- src/main/java/com/chess/Chess.java
+++ src/main/java/com/chess/Chess.java
@@ -9,7 +9,6 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -17,21 +16,20 @@ import javafx.stage.Stage;
 import static com.chess.engine.utils.Constants.Colors.*;
 import static com.chess.engine.utils.Constants.Sizes.*;
 
-
 public class Chess extends Application {
+
     private Game game;
     private HomePage homePage;
     private EndingScreen endingScreen;
-    StackPane root = new StackPane();
-    StackPane gameContent = new StackPane();
+
+    private final StackPane root = new StackPane();
+    private final StackPane gameContent = new StackPane();
 
     @Override
     public void start(Stage primaryStage) {
 
-        homePage = new HomePage(getHostServices());
         game = new Game(update -> onFinished());
-
-        homePage.getPlayButton().setOnMouseClicked(this::removeHomePage);
+        homePage = new HomePage(getHostServices(), update -> onStart());
 
         gameContent.setBackground(new Background(new BackgroundFill(Color.valueOf(BACKGROUND_COLOR), CornerRadii.EMPTY, Insets.EMPTY)));
         gameContent.getChildren().add(game);
@@ -51,36 +49,34 @@ public class Chess extends Application {
         primaryStage.show();
     }
 
-    private void playAgain(Object e) {
-        root.getChildren().remove(endingScreen);
-        root.getChildren().add(homePage);
-        gameContent.getChildren().remove(game);
-        game = new Game(update -> onFinished());
-        gameContent.getChildren().add(game);
-
-    }
-
-    private void removeHomePage(MouseEvent mouseEvent) {
-        root.getChildren().remove(homePage);
-    }
-
     private void keyHandler(KeyEvent e, Stage primaryStage) {
         switch (e.getCode()) {
             case F -> changeFullscreen(primaryStage);
             case Q -> System.exit(0);
         }
     }
-    private void onFinished() {
-        endingScreen = new EndingScreen(game.getWinner());
-        root.getChildren().add(endingScreen);
-        endingScreen.getPlayAgainButton().setOnMouseClicked(this::playAgain);
-    }
-    public static void main(String[] args) {
-        launch(args);
-    }
-
 
     public void changeFullscreen(Stage primaryStage) {
         primaryStage.setFullScreen(!primaryStage.isFullScreen());
+    }
+
+    private void onStart() {
+        root.getChildren().remove(homePage);
+    }
+
+    private void onFinished() {
+        endingScreen = new EndingScreen(game.getWinner(), update -> onRestart());
+        root.getChildren().add(endingScreen);
+    }
+
+    private void onRestart() {
+        gameContent.getChildren().remove(game);
+        game = new Game(update -> onFinished());
+        gameContent.getChildren().add(game);
+        root.getChildren().remove(endingScreen);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
